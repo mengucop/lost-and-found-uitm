c8305cb0d6d66a957a700fc8fe76d287bf9c95b6
mengucop <95490084+mengucop@users.noreply.github.com>
2025-06-22 05:02:13 +0800

    Update layouts.blade.php

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" class="dark">
 <head>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Lost & Found') }}</title>
 
+    {{-- ✅ Load Vite assets --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- ✅ Optional: Font Awesome CDN --}}
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-REPLACE_WITH_REAL_HASH"
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 <body>
     <div class="container">
         @yield('content')
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
